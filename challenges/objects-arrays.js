c38e38150461d169116ddc10913a9e21a26c5ac3
Orie Chinedu <oriechinedu@gmail.com>
2019-05-10 09:37:34 +0100

    (array): Correct wrong solution

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -71,11 +71,11 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = graduates.map(graduate => {
-  return graduate.university;
-}).sort();
-
-console.log(universities)
+const universities = []
+for (let i = 0; i < graduates.length; i++) {
+  universities.push(graduates[i].university);
+}
+console.log(universities.sort())
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -83,16 +83,22 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
-const contactInfo = graduates.map(grad => {
-  return `${grad.first_name} ${grad.email}`
-});
+const contactInfo = []
+for (let i = 0; i < graduates.length; i++) {
+  contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`);
+}
+
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = universities.filter(uni => {
-  return uni.toLowerCase().includes('uni') === true;
-});
+const uni = []
+for (let i = 0; i < universities.length; i++) {
+  if (universities[i].toLowerCase().includes('uni') === true) {
+    uni.push(universities[i]);
+  }
+}
+
 console.log(uni);
 
 
